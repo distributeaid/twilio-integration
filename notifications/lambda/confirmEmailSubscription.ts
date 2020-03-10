f2f414cdd150c30c2f285e1056b47b92b9fe444f
Markus Tacker <m@coderbyheart.com>
2020-03-10 20:21:55 +0100

    feat: send email via SendGrid

--- notifications/lambda/confirmEmailSubscription.ts
+++ notifications/lambda/confirmEmailSubscription.ts
@@ -1,18 +1,21 @@
 import { Context, SNSEvent } from 'aws-lambda'
 import { SSM } from 'aws-sdk'
-import { isLeft, Either } from 'fp-ts/lib/Either'
+import { isLeft, Either, isRight } from 'fp-ts/lib/Either'
 import { ErrorInfo } from '../../appsync/ErrorInfo'
 import {
 	getSendGridSettings,
 	SendGridSettings,
 } from '../../sendgrid/getSendGridSettings'
 import { ChannelSubscriptionCreatedEvent } from '../../events/events'
+import { sendEmail } from '../sendEmail'
 
 const fetchSettings = getSendGridSettings({
 	ssm: new SSM(),
 	scopePrefix: process.env.STACK_NAME as string,
 })
 let sendGridSettings: Promise<Either<ErrorInfo, SendGridSettings>>
+
+const confirmationEndpoint = process.env.CONFIRMATION_ENDPOINT
 
 export const handler = async (event: SNSEvent, _: Context) => {
 	console.log(JSON.stringify({ event }))
@@ -25,22 +28,53 @@ export const handler = async (event: SNSEvent, _: Context) => {
 		return
 	}
 	const { apiKey, domain } = maybeSettings.right
-	console.log({
-		apiKey: apiKey.substr(0, 5) + '*****',
-		domain,
+	console.log(
+		JSON.stringify({
+			apiKey: apiKey.substr(0, 5) + '*****',
+			domain,
+		}),
+	)
+	const s = sendEmail({
+		apiKey,
+		from: {
+			name: 'DistributeAid Chat',
+			email: `toolbox@${domain}`,
+		},
 	})
 
 	const apiEvent = JSON.parse(
 		event.Records[0].Sns.Message,
 	) as ChannelSubscriptionCreatedEvent
 	const { email, uuid } = apiEvent.eventPayload
-	console.log({
-		email,
-		uuid,
-	})
+	const r = await s({
+		to: { email },
+		subject: `[DistributeAid] Please confirm your chat notifications`,
+		text: `
+Hei,
 
-	// FIXME: Send email with confirmation link
-	console.error(
-		`FIXME: Implement sending email to ${email} to confirm subscription ${uuid}!`,
-	)
+please click this link to confirm your email subscription: 
+${confirmationEndpoint}?subscriptionId=${uuid}.
+
+If you did not request to be notified, please simply 
+ignore this message.
+
+Remember: we take privacy and safety very seriously, 
+you can always reach us at hello@distributeaid.org 
+in case you have any questions!
+
+Kind regards,
+Your DistributeAid Platform Team
+		`.trim(),
+	})()
+
+	if (isRight(r)) {
+		console.log(
+			JSON.stringify({
+				email,
+				uuid,
+			}),
+		)
+	} else {
+		console.error(JSON.stringify(r.left))
+	}
 }
